1b999d07517a939159c418ceecc1eb983aedc667
Leonardo Alifraco <leonardo.alifraco@gmail.com>
2017-05-12 12:30:54 -0300

    Add XSRF Token

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 ```
-JSESSIONID=[Session ID here] TARGET_SITE=[Target] node app.js
+XSRF_TOKEN=[XSRF Token here] JSESSIONID=[Session ID here] TARGET_SITE=[Target] node app.js
 ```
--- app.js
+++ app.js
@@ -7,14 +7,15 @@ const proxy = httpProxy.createProxyServer({
   autoRewrite: true,
 });
 
-proxy.on('proxyReq', function(proxyReq, req, res, options) {
-  if (process.env.JSESSIONID) {
-    proxyReq.setHeader('cookie', `JSESSIONID=${process.env.JSESSIONID};path=/`);
+proxy.on('proxyReq', (proxyReq, req, res, options) => {
+  if (process.env.JSESSIONID && process.env.XSRF_TOKEN) {
+    proxyReq.setHeader('cookie', `JSESSIONID=${process.env.JSESSIONID};path=/, atlassian.xsrf.token=${process.env.XSRF_TOKEN};path=/`);
   }
 });
 
-proxy.on('proxyRes', function (proxyRes, req, res) {
+proxy.on('proxyRes', (proxyRes, req, res) => {
   proxyRes.headers['cache-control'] = 'max-age=3600';
 });
 
 proxy.listen(8000);
+	
